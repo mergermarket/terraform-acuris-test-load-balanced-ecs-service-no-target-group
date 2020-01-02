7c4e81629081e5a244fed3ef30994a40f8006dc8
Arthur Gassner <arthur.gassner@gmail.com>
2020-01-02 15:49:57 +0000

    Fix output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,11 @@
 output "name" {
-  value = compact(
-    concat(
-      aws_ecs_service.service.*.name,
-      aws_ecs_service.service_no_loadbalancer.*.name,
+  value = join(
+    "",
+    compact(
+      concat(
+        aws_ecs_service.service.*.name,
+        aws_ecs_service.service_no_loadbalancer.*.name,
+      )
     )
   )
 }
