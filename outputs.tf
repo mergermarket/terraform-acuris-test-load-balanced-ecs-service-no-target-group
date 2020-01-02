a9b938584549d2e358ca125bfc74bbc001092bed
Arthur Gassner <arthur.gassner@gmail.com>
2020-01-02 14:38:11 +0000

    Fix when output is empty - usually during destroy

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,8 @@
 output "name" {
-  value = element(
-    compact(
-      concat(
-        aws_ecs_service.service.*.name,
-        aws_ecs_service.service_no_loadbalancer.*.name,
-      ),
-    ),
-    0,
+  value = compact(
+    concat(
+      aws_ecs_service.service.*.name,
+      aws_ecs_service.service_no_loadbalancer.*.name,
+    )
   )
 }
-
