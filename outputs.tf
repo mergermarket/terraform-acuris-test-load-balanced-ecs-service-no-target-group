51d7fd536ebfa7ce276f0caef1a230c75aa00efc
Keir Badger <keir.badger@iongroup.com>
2022-02-10 10:25:18 +0000

    Correct output to include multiple load balancer service

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,7 @@ output "name" {
         aws_ecs_service.service.*.name,
         aws_ecs_service.service_no_loadbalancer.*.name,
         aws_ecs_service.service_for_awsvpc_no_loadbalancer.*.name,
+        aws_ecs_service.service_multiple_loadbalancers.*.name,
       )
     )
   )
