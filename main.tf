373eeecfbbea26cc2b5fd8293f1d02f10cf4db1a
Keir Badger <keir.badger@iongroup.com>
2024-04-04 13:56:40 +0000

    remove the ignore of capacity provider strategy

--- main.tf
+++ main.tf
@@ -129,7 +129,6 @@ resource "aws_ecs_service" "service_no_loadbalancer" {
   }
   lifecycle {
     ignore_changes = [
-      capacity_provider_strategy,
       ordered_placement_strategy,
     ]
   }
